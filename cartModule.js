8b4d271cacac9d9286e0a9cb18a7d94d198e297c
Nicolás Oficialdegui <nioficialdeg@yahoo.com.ar>
2026-09-17 20:33:38 -0300

    Se agregó función sumatoria al carrito.

--- cartModule.js
+++ cartModule.js
@@ -13,19 +13,33 @@ function actualizarContador() {
     }
 }
 
+function calcularTotal() {
+    let total = 0;
+    
+    carrito.forEach(function(item) {
+        total += parseFloat(item.precio);
+    });
+    
+    return total.toFixed(2);
+}
+
 export function verCarrito() {
-    // Si el carrito está vacío, avisamos
     if (carrito.length === 0) {
         alert("El carrito está vacío.");
         return;
     }
 
-    let mensaje = "Libros en tu carrito:\n\n";
+    let mensaje = "🛒 LIBROS EN TU CARRITO:\n\n";
+    
     carrito.forEach(function(item, indice) {
         mensaje += `${indice + 1}. ${item.nombre} - $${item.precio}\n`;
     });
 
-    mensaje += "\nEscribe el NÚMERO del libro que quieras ELIMINAR (o presiona Cancelar):";
+    const totalAcumulado = calcularTotal();
+    mensaje += `\n-----------------------------------`;
+    mensaje += `\nTOTAL A PAGAR: $${totalAcumulado}`;
+    mensaje += `\n-----------------------------------`;
+    mensaje += `\n\nEscribe el NÚMERO del libro que quieras ELIMINAR (o presiona Cancelar):`;
 
     const respuesta = prompt(mensaje);
 
@@ -33,10 +47,9 @@ export function verCarrito() {
         const numero = parseInt(respuesta);
 
         if (numero > 0 && numero <= carrito.length) {
-            
             const eliminado = carrito.splice(numero - 1, 1);
             alert(`Eliminaste "${eliminado[0].nombre}" del carrito.`);
-            actualizarContador(); 
+            actualizarContador(); // Actualiza el número de la barra
         } else {
             alert("Número no válido.");
         }
